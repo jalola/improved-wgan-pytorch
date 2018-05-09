7aa0c9ff4509cd77fc738aed0273909b45a3cdaa
Hung Nguyen <waterstorm64@gmail.com>
2018-05-09 23:31:26 +0700

    tensor.contiguous().view() --> tensor.reshape

--- models/conwgan.py
+++ models/conwgan.py
@@ -50,10 +50,10 @@ class DepthToSpace(nn.Module):
         output_depth = int(input_depth / self.block_size_sq)
         output_width = int(input_width * self.block_size)
         output_height = int(input_height * self.block_size)
-        t_1 = output.contiguous().view(batch_size, input_height, input_width, self.block_size_sq, output_depth)
+        t_1 = output.reshape(batch_size, input_height, input_width, self.block_size_sq, output_depth)
         spl = t_1.split(self.block_size, 3)
-        stacks = [t_t.contiguous().view(batch_size,input_height,output_width,output_depth) for t_t in spl]
-        output = torch.stack(stacks,0).transpose(0,1).permute(0,2,1,3,4).contiguous().view(batch_size,output_height,output_width,output_depth)
+        stacks = [t_t.reshape(batch_size,input_height,output_width,output_depth) for t_t in spl]
+        output = torch.stack(stacks,0).transpose(0,1).permute(0,2,1,3,4).reshape(batch_size,output_height,output_width,output_depth)
         output = output.permute(0, 3, 1, 2)
         return output
 
